c7b18e8fbc41ebbdbec48988c2e160aa48fa297d
chmilevfa <chmilev.fedor@gmail.com>
2018-07-12 20:45:51 +0200

    Simple logging configuration for log4j with slf4j added

--- src/main/java/net/chmilevfa/telegram/App.java
+++ src/main/java/net/chmilevfa/telegram/App.java
@@ -1,26 +1,35 @@
 package net.chmilevfa.telegram;
 
-import net.chmilevfa.telegram.bots.currency.CurrencyBot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class App {
 
+    private static Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
         ApiContextInitializer.init();
+        registerShutdownHooks();
 
         ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        CurrencyBot currencyBot = (CurrencyBot) context.getBean("currencyBot");
+        TelegramLongPollingBot currencyBot = (TelegramLongPollingBot) context.getBean("currencyBot");
 
         TelegramBotsApi botsApi = new TelegramBotsApi();
         try {
+            logger.info("Registering bots...");
             botsApi.registerBot(currencyBot);
         } catch (TelegramApiException e) {
-            //TODO log and handle
-            e.printStackTrace();
+            logger.error("Error during registering bots", e);
         }
+    }
+
+    private static void registerShutdownHooks() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> logger.info("Shutting down application...")));
     }
 }
