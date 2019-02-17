95fc7dbdca6849c9c82b06e6de9f5ff501643aa0
Katenkka <rootoftheevilest@gmail.com>
2019-02-17 18:44:15 +0100

    #10 removed unnecessary imports

--- src/main/java/net/chmilevfa/telegram/bots/currency/service/impl/FreeCurrencyConverterApiService.java
+++ src/main/java/net/chmilevfa/telegram/bots/currency/service/impl/FreeCurrencyConverterApiService.java
@@ -3,12 +3,10 @@ package net.chmilevfa.telegram.bots.currency.service.impl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.chmilevfa.telegram.bots.currency.Currencies;
 import net.chmilevfa.telegram.bots.currency.service.CurrencyService;
-import org.glassfish.jersey.internal.util.Property;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
